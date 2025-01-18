7637d3aa95b670c80d4026bd28a56e1e22698eef
Chen En <122762161+Ennn-1023@users.noreply.github.com>
2025-01-18 18:50:28 +0800

    Update imagenet.py

--- ldm/data/imagenet.py
+++ ldm/data/imagenet.py
@@ -167,9 +167,10 @@ class ImageNetTrain(ImageNetBase):
             if not os.path.exists(datadir):
                 path = os.path.join(self.root, self.FILES[0])
                 if not os.path.exists(path) or not os.path.getsize(path)==self.SIZES[0]:
-                    import academictorrents as at
-                    atpath = at.get(self.AT_HASH, datastore=self.root)
-                    assert atpath == path
+                    download(self.URL + self.FILES[0], self.root)
+                    # import academictorrents as at
+                    # atpath = at.get(self.AT_HASH, datastore=self.root)
+                    # assert atpath == path
 
                 print("Extracting {} to {}".format(path, datadir))
                 os.makedirs(datadir, exist_ok=True)
