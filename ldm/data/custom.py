445be587805a9cec3b6423a41667394d8d60c3d3
Chen En <122762161+Ennn-1023@users.noreply.github.com>
2025-01-18 14:32:03 +0800

    modify customdataset

--- ldm/data/custom.py
+++ ldm/data/custom.py
@@ -1,18 +1,37 @@
 from torch import utils
 import os
+from PIL import Image
+import numpy as np
+from torchvision import transforms
+
 
 class CustomDataset(utils.data.Dataset):
+    
     def __init__(self, root_dir, transform=None):
         self.root_dir = root_dir
-        self.transform = transform
+        if transform is not None:
+            self.transform = transform 
+        else:
+            self.transform  = transforms.Compose([
+            transforms.Resize((512, 512)),
+            transforms.ToTensor(),
+            ])
         self.data = [os.path.join(root_dir, fname) for fname in os.listdir(root_dir)
                             if fname.endswith(".jpg") or fname.endswith(".png")]
 
     def __getitem__(self, index):
-        x = self.data[index]
+        image = self.data[index]
+        image = Image.open(image)
+        if not image.mode == "RGB":
+            image = image.convert("RGB")
+        image.resize(size=(512, 512))
+        image = np.array(image).astype(np.uint8)
+        
         if self.transform:
-            x = self.transform(x)
-        return x
+            image = self.transform(image)
+        example = {}
+        example['image'] = image
+        return example
 
     def __len__(self):
         return len(self.data)
